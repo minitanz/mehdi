04bd351cc0b5812e31d1af12bd62dac8fa9be4b2
minitanz <botmeselena@gmail.com>
2016-03-08 20:46:55 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {106630608,109890540},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
